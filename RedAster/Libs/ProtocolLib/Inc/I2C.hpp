05d4b22db060dec009d98857c22aeb602af8acf7
alberto-cardini <albycardini@gmail.com>
2024-11-27 22:28:15 +0100

    Fac-simile on how to include HAL symbols

--- RedAster/Libs/ProtocolLib/Inc/I2C.hpp
+++ RedAster/Libs/ProtocolLib/Inc/I2C.hpp
@@ -5,31 +5,14 @@
 #ifndef REDASTER_I2C_HPP
 #define REDASTER_I2C_HPP
 
-//the include section in empty for the moment due to the fact that the pin config has not been done yet
-#include "../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c.h"
-#include <memory>
-#include <vector>
-#include "iostream"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-enum class Mode {
-    NON_BLOCKING, POLLING
-}typedef Mode;
+#include "stm32h7xx.h"
 
-enum class Identity {
-    MASTER, SLAVE
-}typedef Identity;
-
-class I2C {
-public:
-    explicit I2C(I2C_HandleTypeDef init) : parameters(std::make_unique<I2C_HandleTypeDef>(init)) {}
-
-    HAL_StatusTypeDef Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t> data, uint8_t timeout);
-    HAL_StatusTypeDef Receive(Identity role, Mode receive_mode, );
-
-private:
-    std::unique_ptr<I2C_HandleTypeDef> parameters;
-    uint8_t polling_timeout;
-};
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif //REDASTER_I2C_HPP
