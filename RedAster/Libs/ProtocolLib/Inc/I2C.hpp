775e3217ed632a80c7b7ea9668e24eba5a1ad64b
alberto-cardini <albycardini@gmail.com>
2024-11-28 11:50:30 +0100

    Implement I2C

--- RedAster/Libs/ProtocolLib/Inc/I2C.hpp
+++ RedAster/Libs/ProtocolLib/Inc/I2C.hpp
@@ -15,4 +15,21 @@ extern "C" {
 }
 #endif
 
+#include <memory>
+#include <vector>
+#include "Protocol.hpp"
+#include "Utils.hpp"
+
+class I2C : public Protocol{
+public:
+    explicit I2C(I2C_HandleTypeDef init, uint32_t polling_timeout = 0) : handler(std::make_unique<I2C_HandleTypeDef>(init)), polling_timeout(polling_timeout) {}
+
+    HAL_StatusTypeDef Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t>& buffer) const override;
+    HAL_StatusTypeDef Receive(Identity role, Mode receive_mode, std::vector<uint8_t>& buffer) override;
+
+private:
+    std::unique_ptr<I2C_HandleTypeDef> handler;
+    uint8_t polling_timeout;
+};
+
 #endif //REDASTER_I2C_HPP
--- RedAster/Libs/ProtocolLib/Src/I2C.cpp
+++ RedAster/Libs/ProtocolLib/Src/I2C.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "I2C.hpp"
+
+HAL_StatusTypeDef I2C::Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t> &buffer) const {
+    if (role == Identity::MASTER && transmit_mode == Mode::POLLING) {
+        return HAL_I2C_Master_Transmit(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer), polling_timeout);
+    }
+
+    if (role == Identity::MASTER && transmit_mode == Mode::NON_BLOCKING) {
+        return HAL_I2C_Master_Transmit_DMA(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer));
+    }
+
+    if (role == Identity::SLAVE && transmit_mode == Mode::POLLING) {
+        return HAL_I2C_Slave_Transmit(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer), polling_timeout);
+    }
+
+    if (role == Identity::SLAVE && transmit_mode == Mode::NON_BLOCKING) {
+        return HAL_I2C_Slave_Transmit_DMA(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer));
+    }
+
+    else
+        throw std::logic_error("I2C transmission failed");
+}
+
+HAL_StatusTypeDef I2C::Receive(Identity role, Mode receive_mode, std::vector<uint8_t> &buffer) {
+    if (role == Identity::MASTER && receive_mode == Mode::POLLING) {
+        return HAL_I2C_Master_Receive(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer), polling_timeout);
+    }
+
+    if (role == Identity::MASTER && receive_mode == Mode::NON_BLOCKING) {
+        return HAL_I2C_Master_Receive_DMA(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer));
+    }
+
+    if (role == Identity::SLAVE && receive_mode == Mode::POLLING) {
+        return HAL_I2C_Slave_Receive(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer), polling_timeout);
+    }
+
+    if (role == Identity::SLAVE && receive_mode == Mode::NON_BLOCKING) {
+        return HAL_I2C_Slave_Receive_DMA(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer));
+    }
+
+    else
+        throw std::logic_error("I2C transmission failed");
+}
