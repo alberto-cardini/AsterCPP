6442a62f7a4f10d6b3ce38ba3b4e754deb50f5b8
alberto-cardini <albycardini@gmail.com>
2024-11-28 11:50:23 +0100

    Implement SPI

--- RedAster/Libs/ProtocolLib/Inc/SPI.hpp
+++ RedAster/Libs/ProtocolLib/Inc/SPI.hpp
@@ -5,9 +5,32 @@
 #ifndef REDASTER_SPI_HPP
 #define REDASTER_SPI_HPP
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-class SPI {
+#include "stm32h7xx.h"
 
+#ifdef __cplusplus
+}
+#endif
+
+#include <memory>
+#include <vector>
+#include "iostream"
+#include "Utils.hpp"
+#include "Protocol.hpp"
+
+class SPI : public Protocol{
+public:
+    explicit SPI(SPI_HandleTypeDef init, uint32_t polling_timeout = 0) : handler(std::make_unique<SPI_HandleTypeDef>(init)), polling_timeout(polling_timeout) {}
+
+    HAL_StatusTypeDef Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t>& buffer) const override;
+    HAL_StatusTypeDef Receive(Identity role, Mode receive_mode, std::vector<uint8_t>& buffer) override;
+
+private:
+    std::unique_ptr<SPI_HandleTypeDef> handler;
+    uint8_t polling_timeout;
 };
 
 
--- RedAster/Libs/ProtocolLib/Src/SPI.cpp
+++ RedAster/Libs/ProtocolLib/Src/SPI.cpp
@@ -2,4 +2,31 @@
 // Created by Alberto Cardini on 21/11/24.
 //
 
-#include "../Inc/SPI.hpp"
+#include "SPI.hpp"
+
+
+HAL_StatusTypeDef SPI::Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t> &buffer) const {
+    if (role == Identity::N_A && transmit_mode == Mode::POLLING) {
+        return HAL_SPI_Transmit(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer), polling_timeout);
+    }
+
+    if (role == Identity::N_A && transmit_mode == Mode::NON_BLOCKING) {
+        return HAL_SPI_Transmit_DMA(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer));
+    }
+
+    else
+        throw std::logic_error("Non-valid identity for the SPI Transmit operation");
+}
+
+HAL_StatusTypeDef SPI::Receive(Identity role, Mode receive_mode, std::vector<uint8_t> &buffer) {
+    if (role == Identity::N_A && receive_mode == Mode::POLLING) {
+        return HAL_SPI_Receive(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer), polling_timeout);
+    }
+
+    if (role == Identity::N_A && receive_mode == Mode::NON_BLOCKING) {
+        return HAL_SPI_Receive_DMA(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer));
+    }
+
+    else
+        throw std::logic_error("Non-valid identity for the SPI Transmit operation");
+}
