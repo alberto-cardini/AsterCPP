47d22709aa0cbb9b87f5fa33a395643ab127b50b
danimorgan00 <daniele.morganti07@gmail.com>
2024-12-15 17:27:27 +0100

    Some comments in the class definition added

--- RedAster/Libs/ProtocolLib/Inc/SPILink.h
+++ RedAster/Libs/ProtocolLib/Inc/SPILink.h
@@ -9,6 +9,31 @@
 #include "Link.h"
 #include "SPIChannel.hpp"
 
+/*
+ * SPILink.h
+ *
+ * Definition of SPILink class, which implements a link interface for an
+ * SPI communication. The class uses an SPIChannel object to facilitate communication
+ * and provides methods for transmitting data over an SPI bus.
+ * It also manages the chip select (CS) pin to enable and disable communication with
+ * the connected device. More than one instance of this class can use the same channel.
+ *
+ * Dependencies:
+ * - HAL library.
+ * - SPIChannel.hpp.
+ *
+ * Notes:
+ * - Methods for receiving (Receive) and bidirectional transmission (TransmitReceive) are not
+ *   yet implemented (marked as TODO).
+ *
+ * Usage example:
+ *
+ * SPIChannel channel(&handler, 10);
+ * SPILink spiLink(&channel, GPIOA, GPIO_PIN_4, Mode::POLLING);
+ * std::vector<uint8_t> data = {0x01, 0x02, 0x03};
+ * HAL_StatusTypeDef status = spiLink.Transmit(data);
+ *
+ */
 class SPILink : public Link{
 public:
     SPILink(SPIChannel& channel, GPIO_TypeDef* csPort, uint16_t csPin, Mode modeTransmit)
